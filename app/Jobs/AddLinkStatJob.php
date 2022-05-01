ee73efa5429775acd633e498b126b5fcecc3f031
Max Denisiyk <md@it-agency.ru>
2022-05-01 08:19:31 +0300

    Job для добавления статистики при переходе на ссылку

---
+++ app/Jobs/AddLinkStatJob.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Services\Stat\DTO\StatAddDTO;
+use App\Services\Stat\StatService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class AddLinkStatJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /** @var StatAddDTO */
+    private StatAddDTO $statData;
+
+    public function __construct(StatAddDTO $statData)
+    {
+        $this->statData = $statData;
+    }
+
+    /**
+     * @param StatService $statService
+     * @return void
+     */
+    public function handle(StatService $statService): void
+    {
+        $statService->storeToDB($this->statData);
+    }
+}
